d63c00165422fa6c1a5dde5ac69f02f2bb6b3432
Justin Phelps <linuturk@onitato.com>
2015-06-26 12:37:40 -0500

    purging chef

--- site-cookbooks/patch-drupalconf/metadata.rb
+++
@@ -1,9 +0,0 @@
-maintainer 'Jason Boyles'
-maintainer_email 'jason.boyles@rackspace.com'
-license 'Apache 2.0'
-description 'Patch drupal.conf apache config file'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.1'
-name 'patch-drupalconf'
-
-depends 'drupal'
